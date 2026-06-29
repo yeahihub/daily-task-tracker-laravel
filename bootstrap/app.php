1aabd82fcf1b5406535d8602325ba3879738725a
Boris Shapovalov <zhabkinbread@gmail.com>
2026-06-29 19:03:56 +0300

    Deleted trusted hosts in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function(Middleware $middleware): void {
-        $middleware->trustHosts();
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
     })
-    ->withExceptions(function(Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
