ee284561e0b669fb302940962a82eb7d5c9b6a30
Boris Shapovalov <zhabkinbread@gmail.com>
2026-06-29 20:05:10 +0300

    Added support for https requests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Database\LazyLoadingViolationException;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,69 +31,73 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        CarbonImmutable::setLocale(config('app.locale'));
-        setlocale(LC_TIME, 'ru_RU.UTF-8');
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
 
-        $loginRateLimitResponse = function (Request $request) {
-            if ($request->expectsJson()) {
-                return response()->json(
-                    [
-                        'message' => 'Too many login attempts. Please try again later.',
-                    ],
-                    429
-                );
-            }
+            CarbonImmutable::setLocale(config('app.locale'));
+            setlocale(LC_TIME, 'ru_RU.UTF-8');
 
-            return back()
-                ->withErrors(['email' => 'Too many login attempts. Please try again later.'])
-                ->withInput($request->except('password'));
-        };
+            $loginRateLimitResponse = function (Request $request) {
+                if ($request->expectsJson()) {
+                    return response()->json(
+                        [
+                            'message' => 'Too many login attempts. Please try again later.',
+                        ],
+                        429
+                    );
+                }
 
-        RateLimiter::for(
-            'login',
-            fn(Request $request) => [
-                Limit::perMinute(100)->by($request->ip())->response($loginRateLimitResponse),
+                return back()
+                    ->withErrors(['email' => 'Too many login attempts. Please try again later.'])
+                    ->withInput($request->except('password'));
+            };
 
-                Limit::perMinute(5)->by($request->input('email'))->response($loginRateLimitResponse),
-            ]
-        );
+            RateLimiter::for(
+                'login',
+                fn(Request $request) => [
+                    Limit::perMinute(100)->by($request->ip())->response($loginRateLimitResponse),
 
-        RateLimiter::for('password-reset-request', fn(Request $request) => [
-            Limit::perHour(10)->by($request->ip()),
-            Limit::perHour(3)->by($request->input('email')),
-        ]);
+                    Limit::perMinute(5)->by($request->input('email'))->response($loginRateLimitResponse),
+                ]
+            );
 
-        RateLimiter::for('password-reset', fn(Request $request) => [
-            Limit::perHour(5)->by($request->ip()),
-            Limit::perHour(3)->by($request->input('email')),
-        ]);
+            RateLimiter::for('password-reset-request', fn(Request $request) => [
+                Limit::perHour(10)->by($request->ip()),
+                Limit::perHour(3)->by($request->input('email')),
+            ]);
 
-        Password::defaults(function () {
-            if ($this->app->isLocal()) {
-                return Password::min(8);
-            }
+            RateLimiter::for('password-reset', fn(Request $request) => [
+                Limit::perHour(5)->by($request->ip()),
+                Limit::perHour(3)->by($request->input('email')),
+            ]);
 
-            return Password::min(8)
-                ->mixedCase()
-                ->uncompromised()
-                ->letters()
-                ->numbers()
-                ->symbols();
-        });
+            Password::defaults(function () {
+                if ($this->app->isLocal()) {
+                    return Password::min(8);
+                }
 
-        Model::shouldBeStrict();
+                return Password::min(8)
+                    ->mixedCase()
+                    ->uncompromised()
+                    ->letters()
+                    ->numbers()
+                    ->symbols();
+            });
 
-        Model::handleLazyLoadingViolationUsing(function ($model, $relation): void {
-            $class = get_class($model);
+            Model::shouldBeStrict();
 
-            if (app()->isLocal()) {
-                throw new LazyLoadingViolationException($model, $relation);
-            }
+            Model::handleLazyLoadingViolationUsing(function ($model, $relation): void {
+                $class = get_class($model);
 
-            info('Attempted to lazy load "' . $relation . '" on model "' . $class . '"');
-        });
+                if (app()->isLocal()) {
+                    throw new LazyLoadingViolationException($model, $relation);
+                }
 
-        DB::prohibitDestructiveCommands(app()->isProduction());
-        Date::use(CarbonImmutable::class);
+                info('Attempted to lazy load "' . $relation . '" on model "' . $class . '"');
+            });
+
+            DB::prohibitDestructiveCommands(app()->isProduction());
+            Date::use(CarbonImmutable::class);
+        }
     }
 }
